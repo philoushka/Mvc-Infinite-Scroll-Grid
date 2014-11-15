d9bf06e14c6868e1ced8b687495467033c479fa7
beetletun <pzapanta@outlook.com>
2014-11-15 16:34:19 -0500

    Unordered list sample

---
+++ Demo/Controllers/UlController.cs
@@ -0,0 +1,59 @@
+﻿using MvcInfiniteScrollGridDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcInfiniteScrollGridDemo.Controllers
+{
+    public class UlController : Controller
+    {
+        public const int RecordsPerPage = 20;
+
+        public UlController()
+        {
+            ViewBag.RecordsPerPage = RecordsPerPage;
+        }
+        // GET: Ul
+        public ActionResult Index(int? pageNum)
+        {
+            pageNum = pageNum ?? 0;
+            ViewBag.IsEndOfRecords = false;
+            if (Request.IsAjaxRequest())
+            {
+                var customers = GetRecordsForPage(pageNum.Value);
+                ViewBag.IsEndOfRecords = (customers.Any()) && ((pageNum.Value * RecordsPerPage) >= customers.Last().Key);
+                return PartialView("_CustomerLi", customers);
+            }
+            else
+            {
+                LoadAllCustomersToSession();
+                ViewBag.Customers = GetRecordsForPage(pageNum.Value);
+                return View("Index");
+            }
+        }
+
+        public void LoadAllCustomersToSession()
+        {
+            var customerRepo = new CustomerRepository();
+            var customers = customerRepo.ListCustomers();
+            int custIndex = 1;
+            Session["Customers"] = customers.ToDictionary(x => custIndex++, x => x);
+            ViewBag.TotalNumberCustomers = customers.Count();
+        }
+
+        public Dictionary<int, Customer> GetRecordsForPage(int pageNum)
+        {
+            Dictionary<int, Customer> customers = (Session["Customers"] as Dictionary<int, Customer>);
+
+            int from = (pageNum * RecordsPerPage);
+            int to = from + RecordsPerPage;
+
+            return customers
+                .Where(x => x.Key > from && x.Key <= to)
+                .OrderBy(x => x.Key)
+                .ToDictionary(x => x.Key, x => x.Value);
+        }
+    }
+}
